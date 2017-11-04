bec97e12698872afd04ac7a6617b799b56baf0ce
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-11-04 19:59:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,5 +39,14 @@ class Song
   end
 
   def artist_count
+    artist_count = {}
+    @@artists.each do |genre|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count    
   end
 end
